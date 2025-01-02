b0e5fa72ac095e7dd442770074233d1c27094ced
ismailkolawola <ismailkolawola@gmail.com>
2025-01-02 12:20:23 +0100

    add fixes

--- contact.js
+++ contact.js
@@ -1,13 +1,13 @@
-// variables
-const form = document.getElementById('form');
-const nameInput = document.getElementById('name');
-const emailInput = document.getElementById('email');
-const password1 = document.getElementById('password1');
-const password2= document.getElementById('password2');
+// // variables
+// const form = document.getElementById('form');
+// const nameInput = document.getElementById('name');
+// const emailInput = document.getElementById('email');
+// const password1 = document.getElementById('password1');
+// const password2= document.getElementById('password2');
 
-//Error variables
-const nameError = document.getElementById('nameError');
-const form = document.getElementById('form');
-const form = document.getElementById('form');
-const form = document.getElementById('form');
-const form = document.getElementById('formError');
+// //Error variables
+// const nameError = document.getElementById('nameError');
+// const form = document.getElementById('form');
+// const form = document.getElementById('form');
+// const form = document.getElementById('form');
+// const form = document.getElementById('formError');
