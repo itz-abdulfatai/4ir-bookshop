d0a49d2530ce135ce4e969d633c64e5ad79a3cb0
itz_abdulfatai <134066346+itz-abdulfatai@users.noreply.github.com>
2025-01-03 08:22:36 +0100

    add navigation

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+const nav = document.getElementById('naav')
+const menu = document.getElementById('menuu')
 window.addEventListener('scroll', function () {
     const header = document.getElementById('my-header')
+    nav.classList.remove('active')
+
 
     if (window.scrollY > 500) {
 
@@ -10,4 +14,13 @@ window.addEventListener('scroll', function () {
     }
 
 
+})
+
+
+// console.log(nav)
+// console.log(menu)
+
+
+menu.addEventListener('click', function () {
+    nav.classList.toggle('active')
 })
